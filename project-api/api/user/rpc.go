ea113c798afb0e1e9713967ba15a240108f4afa8
yuyansong <1259581033@qq.com>
2025-03-20 22:04:24 +0800

    docker go程序部署

--- project-api/api/user/rpc.go
+++ project-api/api/user/rpc.go
@@ -1,25 +1,18 @@
 package user
 
 import (
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/resolver"
-	"log"
-	"project-common/discovery"
-	"project-common/logs"
-	"project-user/config"
 	loginServiceV1 "project-user/pkg/service/login.service.v1"
 )
 
 var LoginServiceClient loginServiceV1.LoginServiceClient
 
 func InitRpcUserClient() {
-	etcdRegister := discovery.NewResolver(config.C.EtcdConfig.Addrs, logs.LG)
-	resolver.Register(etcdRegister)
-
-	conn, err := grpc.Dial("etcd:///user", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	LoginServiceClient = loginServiceV1.NewLoginServiceClient(conn)
+	//etcdRegister := discovery.NewResolver(config.C.EtcdConfig.Addrs, logs.LG)
+	//resolver.Register(etcdRegister)
+	//
+	//conn, err := grpc.Dial("etcd:///user", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//if err != nil {
+	//	log.Fatalf("did not connect: %v", err)
+	//}
+	//LoginServiceClient = loginServiceV1.NewLoginServiceClient(conn)
 }
