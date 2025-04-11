f4739cb835541922ee05fe8913de08fb5073838a
yuyansong <1259581033@qq.com>
2025-04-11 08:01:08 +0800

    调整密钥

--- project-common/encrypts/encrypts_test.go
+++ project-common/encrypts/encrypts_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestEncrypt(t *testing.T) {
-	plain := "100123213123"
+	plain := "5"
 	// AES 规定有3种长度的key: 16, 24, 32分别对应AES-128, AES-192, or AES-256
-	key := "abcdefgehjhijkmlkjjwwoew"
+	key := "sdfgyrhgbxcdgryfhgywertd"
 	// 加密
 	cipherByte, err := Encrypt(plain, key)
 	if err != nil {
